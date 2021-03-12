0e5cb999b43f2f368f4ed6ff81e8ce8ed79cc403
Pedro <pgg1998@gmail.co>
2021-03-11 20:50:17 -0800

    Added .env file to .gitignore

--- client/src/apis/ProductFinder.js
+++ client/src/apis/ProductFinder.js
@@ -1,6 +1,10 @@
 // This file returns a axios object that allows us to query our database with .get, .put, etc.
 import axios from "axios";
 
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "api/v1/products"
+    : "http://localhost:3002/api/v1/products";
 export default axios.create({
-  baseURL: "http://localhost:3002/api/v1/products",
+  baseURL: baseURL,
 });
